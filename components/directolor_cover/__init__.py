9d572908a255d992325e675c305ff2a74a91b0b8
loucks1 <jason.a.loucks@gmail.com>
2025-04-16 13:25:21 -0500

    dd

--- components/directolor_cover/__init__.py
+++ components/directolor_cover/__init__.py
@@ -1,11 +1,1 @@
-import esphome.codegen as cg
-import esphome.components.button as button
-from esphome.const import ICON_RESTART
-
-dummy_button = button.button.Schema(
-    {
-        cg.CONF_ID: "dummy_join_button",
-        button.CONF_ICON: ICON_RESTART,
-        button.CONF_NAME: "Dummy Join Button",
-    }
-)
+import esphome.components.button  # ✅ Forces button component to load and compile
