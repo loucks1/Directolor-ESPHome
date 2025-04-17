69d9e8b3a5f40bb3407f1eb65c83362f94165503
loucks1 <jason.a.loucks@gmail.com>
2025-04-17 12:15:55 -0500

    ddd

--- components/directolor_cover/directolor_cover.cpp
+++ components/directolor_cover/directolor_cover.cpp
@@ -76,14 +76,9 @@ namespace esphome
             App.register_button(this->remove_button_);
         }
 
-        void DirectolorCover::ActionButton::press_action()
-        {
-            this->parent_->on_action_button_press();
-        }
-
         void DirectolorCover::on_action_button_press()
         {
-            ESP_LOGD(TAG, "Join button pressed");
+            ESP_LOGD(TAG, "Action button pressed");
         }
 
         void DirectolorCover::control(const cover::CoverCall &call)
