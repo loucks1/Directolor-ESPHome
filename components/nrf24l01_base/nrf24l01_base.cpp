7f4b6eb15eef67e2acd4664dfc70c133486dcae2
loucks1 <jason.a.loucks@gmail.com>
2025-04-23 10:55:00 -0500

    ddd

--- components/nrf24l01_base/nrf24l01_base.cpp
+++ components/nrf24l01_base/nrf24l01_base.cpp
@@ -176,12 +176,12 @@ namespace esphome
               return;
 #endif
 
-            if (payload[4] != 0xFF || payload[5] != 0xFF || payload[6] != this->remoteCode.radioCode[2] || payload[7] != this->remoteCode.radioCode[3] || payload[8] != 0x86)
-              return;
-
             ESP_LOGD(TAG, "bytes: %d pipe: %d: %s", bytes - 1, pipe, this->formatHex(payload, 0, bytes, " ").c_str());
             const char *command = "ERROR";
 
+            if (payload[4] != 0xFF || payload[5] != 0xFF || payload[6] != this->remoteCode.radioCode[2] || payload[7] != this->remoteCode.radioCode[3] || payload[8] != 0x86)
+              return;
+d
             switch (payload[bytes - 5])
             {
             case directolor_open:
